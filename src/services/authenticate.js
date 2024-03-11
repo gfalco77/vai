9b3eb1d7729cbb89967fc44773b065014cb83a36
gfalco77 <giuseppefalco77@gmail.com>
2024-03-11 08:53:37 +0000

    refactor userpool and using yarn in the workflow

--- src/services/authenticate.js
+++ src/services/authenticate.js
@@ -1,6 +1,10 @@
 import { AuthenticationDetails, CognitoUser, CognitoUserAttribute } from 'amazon-cognito-identity-js';
-import userPool from '../UserPool';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
+const userPool = new CognitoUserPool({
+  UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_CLIENT_ID
+});
 export const createUser = (email, password, firstname, lastname, dateOfBirth, phoneNumber, locale, callback) => {
   const attributeList = [
     new CognitoUserAttribute({
